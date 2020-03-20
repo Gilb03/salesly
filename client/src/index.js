d30c3a84341402f1b959b503956d46e1d21ba6a6
Gilbert King <gilbertking@Gilberts-MacBook-Pro-2.local>
2020-03-20 18:43:15 -0500

    refactor #1

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import {BrowserRouter as Router} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
